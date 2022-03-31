67cc5969301d24b43639c0a30cd47b877c8bda8e
Michael Canan <rebelsmiley11@gmail.com>
2022-03-31 18:46:41 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,11 +1,11 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
+module "s3-bucket" {
+  source  = "app.terraform.io/michaelcanan/s3-bucket/aws"
+  version = "2.8.0"
+  # insert required variables here
   bucket = "michael-canan"
   acl    = "private"
 
   versioning = {
     enabled = true
   }
-
 }
